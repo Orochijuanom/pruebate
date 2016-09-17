639123d1a43cb77492d8117343c4b987c99b9d2f
arbeyjimenez <arbeyjimenez@gmail.com>
2016-09-16 21:07:32 -0500

    Sincronizando cambios

--- app/Http/Controllers/DocenteController.php
+++ app/Http/Controllers/DocenteController.php
@@ -8,25 +8,8 @@ use App\Http\Requests;
 
 class DocenteController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return view('docente.index');
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+    
+    
 
     /**
      * Store a newly created resource in storage.
