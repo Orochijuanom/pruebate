d5d6a168cb554e51d78074c37fb132f29e7174b1
Juan Sebastian Cruz Perdomo <jcruz02@itfip.edu.co>
2016-10-05 21:09:12 -0500

    cambios

--- resources/views/administrador/index.blade.php
+++ resources/views/administrador/index.blade.php
@@ -56,6 +56,19 @@
                 </div>
             </div>
 
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img src="{{asset('img/estudiantes.png')}}" alt="..."/>
+                    <div class="caption">
+                        <h3>Estudiantes</h3>
+                        <p>Carga los Estudiantes al sistema</p>
+                        <p>
+                            <a href="/administrador/estudiantes" class="btn btn-primary" role="button">Entrar</a> 
+                        </p>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>    
 </div>
